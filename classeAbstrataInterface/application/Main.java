d97f137e5496c95e562834320cfdc47c3b941f58
Ivanilson Alves <141331100+ivanalves-t@users.noreply.github.com>
2024-06-10 20:42:54 -0300

    Update Main.java

--- classeAbstrataInterface/application/Main.java
+++ classeAbstrataInterface/application/Main.java
@@ -8,13 +8,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		CarbonFootprint[] cfpList = new CarbonFootprint[2];
+		CarbonFootprint[] cfpList = new CarbonFootprint[6];
 		
 		cfpList[0] = new Car("Ciena", false, 250);
-		cfpList[1] = new Bicycle("Monark", true);
+		cfpList[1] = new Car("Ford", true, 400);
+		cfpList[2] = new Bicycle("Monark", true);
+		cfpList[3] = new Bicycle("BMX", false);
+		cfpList[4] = new Building(3, true, 5);
+		cfpList[5] = new Building(7, false, 12);
 
 		for (CarbonFootprint c : cfpList) {
-			System.out.println(c.getClass().getSimpleName() +", "+ c.getCarbonFootprint());
+			System.out.println(c.getClass().getSimpleName() + ", "+ c.getCarbonFootprint());
 		}
 	}
 
