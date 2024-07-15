cc2578832d4ea24307f59c4fa29f28fd61dc5a64
Ivanilson Alves <141331100+ivanalves-t@users.noreply.github.com>
2024-07-15 20:03:07 -0300

    Bugfix main.Java

--- classeAbstrataInterface/application/Main.java
+++ classeAbstrataInterface/application/Main.java
@@ -11,22 +11,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		CarbonFootprint[] cfpList = new CarbonFootprint[6];
-<<<<<<< HEAD
 
-=======
-		
->>>>>>> d97f137e5496c95e562834320cfdc47c3b941f58
 		cfpList[0] = new Car("Ciena", false, 250);
 		cfpList[1] = new Car("Ford", true, 400);
 		cfpList[2] = new Bicycle("Monark", true);
 		cfpList[3] = new Bicycle("BMX", false);
-<<<<<<< HEAD
 		cfpList[4] = new House(3, true, 5);
 		cfpList[5] = new School(7, false, 12, 10);
-=======
 		cfpList[4] = new Building(3, true, 5);
 		cfpList[5] = new Building(7, false, 12);
->>>>>>> d97f137e5496c95e562834320cfdc47c3b941f58
 
 		for (CarbonFootprint c : cfpList) {
 			System.out.println(c.getClass().getSimpleName() + ", "+ c.getCarbonFootprint());
